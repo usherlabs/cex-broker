aa5642a0cdc413c0cadd6dc9af7a14936f16190a
Abhishekkochar <abhishekkochar2@gmail.com>
2025-08-19 14:11:34 +0800

    Fix: serialization issue with orderbook
    Signed-off-by: Abhishekkochar <abhishekkochar2@gmail.com>

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -3,6 +3,7 @@ import { Logger } from "tslog";
 const log = new Logger({
 	type: process.env.NODE_ENV === "production" ? "json" : "pretty",
 	stylePrettyLogs: false,
+	minLevel: process.env.LOG_LEVEL === "debug" ? 0 : 3,
 });
 
 export { log };
